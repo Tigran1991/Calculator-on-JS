03a1ba600a35a858989eab952e78773aeb79efec
Tigran Aginyan <taginyan91@gmail.comm>
2022-12-25 09:27:29 +0400

    fix adding event listeners on buttons

--- script.js
+++ script.js
@@ -1,12 +1,39 @@
-const actionBtns = document.getElementsByClassName("actionBtn")
-const digitalBtns = document.getElementsByClassName("digitalBtn")
-const resultButton = document.querySelector("#btn_result")
+const actionBtns = document.getElementsByClassName("btn action")
+const digitalBtns = document.getElementsByClassName("btn digital")
+const buttons = document.getElementsByClassName("btn")
+const resultBtn = document.querySelector("#btn_result")
 const deleteBtn = document.querySelector("#btn_delete")
 const switchThemeText = document.querySelector("#switch_theme_text")
 const calculator = document.querySelector("#calculator")
 const switchTheme = document.querySelector("#switch_theme")
 const enteredValue = document.querySelector("#entered_actions")
 const resultField = document.querySelector("#result_field")
+
+const addClickEventOnBtns = () => {
+  const getButtonsValue = (e) => {
+    enteredValue.value += e.target.value
+  }
+
+  for (const button of buttons) {
+    button.addEventListener("click", getButtonsValue)
+  }
+}
+
+addClickEventOnBtns()
+
+const getResult = () => {
+  const enteredCurrentValue = enteredValue.value
+  if (enteredCurrentValue.includes("%")) {
+    const enteredUpdatedValue = enteredCurrentValue.replace("%", "*") + "/100"
+    const result = eval(enteredUpdatedValue)
+    return (resultField.textContent = `= ${result}`)
+  } else {
+    const result = eval(enteredCurrentValue)
+    return (resultField.textContent = `= ${result}`)
+  }
+}
+
+resultBtn.addEventListener("click", getResult)
 
 const changeStyles = (background, color) => {
   calculator.style.background = background
@@ -19,7 +46,7 @@ const changeStyles = (background, color) => {
   deleteBtn.style.border = `2px solid ${color}`
   deleteBtn.style.color = color
   switchThemeText.style.color = color
-  resultButton.style.border = `2px solid ${color}`
+  resultBtn.style.border = `2px solid ${color}`
 }
 
 const changeCalculatorBackground = () => {
@@ -34,21 +61,5 @@ const changeCalculatorBackground = () => {
     )
   } else if (switchTheme.checked) {
     changeStyles("rgb(202, 184, 184)", "#333")
-  }
-}
-
-const getButtonsValue = (value) => {
-  enteredValue.value += value
-}
-
-const getResult = () => {
-  const enteredCurrentValue = enteredValue.value
-  if (enteredCurrentValue.includes("%")) {
-    const enteredUpdatedValue = enteredCurrentValue.replace("%", "*") + "/100"
-    const result = eval(enteredUpdatedValue)
-    return (resultField.textContent = `= ${result}`)
-  } else {
-    const result = eval(enteredCurrentValue)
-    return (resultField.textContent = `= ${result}`)
   }
 }
